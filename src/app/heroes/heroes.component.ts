bcb7441beec63a6e1d3eba2d8964d5e3a05e5a2c
Ironildo-Jr <ironildojr.1@gmail.com>
2022-05-12 17:57:34 -0300

    funcionalidade de selecionar o heroi

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,15 +7,21 @@ import { Hero } from '../hero.model';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [
-    { id: 1, name: 'Wolverine' },
-    { id: 2, name: 'Noturno' },
-    { id: 3, name: 'Tempesta' },
-    { id: 4, name: 'Ciclope' },
-  ];
-  hero: Hero = { id: 0, name: 'Wolverine' };
+  heroes: Hero[] = [];
+  hero?: Hero;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.heroes = [
+      { id: 1, name: 'Wolverine' },
+      { id: 2, name: 'Noturno' },
+      { id: 3, name: 'Tempesta' },
+      { id: 4, name: 'Ciclope' },
+    ];
+  }
+
+  selectHero(hero: Hero) {
+    this.hero = hero;
+  }
 }
