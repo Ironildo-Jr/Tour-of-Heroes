5205ce9abf3b773f93fb5192022aebdcf7b66830
Ironildo-Jr <ironildojr.1@gmail.com>
2022-05-14 10:38:56 -0300

    adicionando mensagens

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from './hero.model';
+import { MessageService } from './message.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,16 +10,22 @@ import { Hero } from './hero.model';
 export class HeroService {
   url: string = 'http://localhost:3001/heroes';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private messageService: MessageService
+  ) {}
 
   getAll() {
+    this.messageService.add('HeroService: fetched heroes')
     return this.httpClient.get<Hero[]>(this.url);
   }
 
   getById(id: Number) {
+    this.messageService.add(`HeroService: fetched id ${id}`)
     return this.httpClient.get<Hero>(`${this.url}/${id}`);
   }
   save(hero: Hero) {
+    this.messageService.add('HeroService: saved hero')
     if (hero.id) {
       return this.httpClient.put<Hero>(`${this.url}/${hero.id}`, hero);
     }
